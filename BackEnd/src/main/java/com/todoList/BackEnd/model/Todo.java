12f4dfa35502b1ad1c24b1dd206d0a32c829fadd
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2025-10-06 00:26:49 -0300

    adição do pacote modelo e criação do model Todo

---
+++ BackEnd/src/main/java/com/todoList/BackEnd/model/Todo.java
@@ -0,0 +1,15 @@
+package com.todoList.BackEnd.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Todo {
+    @Id
+    private String id;
+    private String task;
+    private boolean completed;
+
+}
