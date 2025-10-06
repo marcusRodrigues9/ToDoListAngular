5cddfcbcce6d1845b341531ba1a824dc1adffcc3
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2025-10-06 01:16:20 -0300

    ..

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,45 +1,49 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TodoService } from './todo.service';
 export interface TodoItem{
-  id:number;
+  id?:string;
   task:string;
   completed: boolean;
 
 }
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   todoList: TodoItem[]= [];
   newTask: string="";
+  constructor(private todoService: TodoService){}
 
   ngOnInit(){
-    const saved = localStorage.getItem('todoList');
-    if(saved){
-      this.todoList= JSON.parse(saved)
-    }
+    this.todoService.getTask().subscribe(tasks => this.todoList= tasks)
   }
   addTask():void{
     if(this.newTask.trim()!== ''){
-      const newTodoItem: TodoItem = {
-        id: Date.now(),
+      const task: TodoItem = {
         task: this.newTask,
         completed: false
       }
-      this.todoList.push(newTodoItem)
+      this.todoService.addTask(task).subscribe(created => {
+      this.todoList.push(created)
       this.newTask=''
+      })
     }
-    localStorage.setItem('todoList', JSON.stringify(this.todoList))
   }
-  toggleCompleted(index:number):void{
-    this.todoList[index].completed = !this.todoList[index].completed
-    localStorage.setItem('todoList', JSON.stringify(this.todoList))
-  }
+toggleCompleted(index: number): void {
+  const task = this.todoList[index];
+  task.completed = !task.completed;
+
+  this.todoService.updateTask(task).subscribe(updatedTask => {
+    this.todoList[index] = updatedTask;
+  });
+}
 
   deleteTask(id:number):void{
     this.todoList= this.todoList.filter(item => item.id !== id)
---
+++ frontend/src/app/todo.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { TodoItem } from "./app";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({providedIn:'root'})
+export class TodoService{
+    
+    private baseUrl = 'http://localhost:8080/todos/api';
+    constructor(private http: HttpClient){}
+
+    getTask(): Observable<TodoItem[]>{
+        return this.http.get<TodoItem[]>(this.baseUrl);
+    }
+    addTask(task: TodoItem): Observable<TodoItem>{
+        return this.http.post<TodoItem>(this.baseUrl, task);
+    }
+    deleteTask(id: string): Observable<void>{
+        return this.http.delete<void>(`${this.baseUrl}/${id}}`)
+    }
+    updateTask(task: TodoItem): Observable<TodoItem>{
+        return this.http.put<TodoItem>(`${this.baseUrl}/${task.id}}`, task)
+    }
+}
