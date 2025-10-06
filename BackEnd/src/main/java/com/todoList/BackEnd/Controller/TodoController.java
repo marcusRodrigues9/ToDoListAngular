b09ac373f17fd58f6f222c23f832cf6ada570f2b
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2025-10-06 00:46:47 -0300

    finalização do controller

--- BackEnd/src/main/java/com/todoList/BackEnd/Controller/TodoController.java
+++ BackEnd/src/main/java/com/todoList/BackEnd/Controller/TodoController.java
@@ -1,12 +1,36 @@
 package com.todoList.BackEnd.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.todoList.BackEnd.Repository.TodoRepository;
+import com.todoList.BackEnd.model.Todo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/todos/api")
 @CrossOrigin(origins = "http://localhost:4200")
 public class TodoController {
 
+    private final TodoRepository repository;
+    public TodoController(TodoRepository repository) {
+        this.repository = repository;
+    }
+    @GetMapping
+    public List<Todo> getAll(){
+        return repository.findAll();
+    }
+    @PostMapping
+    public Todo create(@RequestBody Todo todo){
+        return repository.save((todo));
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id){
+        repository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Todo update(@PathVariable String id, @RequestBody Todo todo){
+        todo.setId(id);
+        return repository.save(todo);
+    }
 }
--- BackEnd/src/main/java/com/todoList/BackEnd/model/Todo.java
+++ BackEnd/src/main/java/com/todoList/BackEnd/model/Todo.java
@@ -1,15 +1,19 @@
 package com.todoList.BackEnd.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
 @Document
+@Data
 public class Todo {
     @Id
     private String id;
     private String task;
     private boolean completed;
 
+    public void setId(String id) {
+    }
 }
