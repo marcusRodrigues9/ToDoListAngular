b5c2be10f06b9fc1de32c62b0a26af54ab91f30c
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2025-10-05 23:55:25 -0300

    adiçao back

---
+++ BackEnd/src/test/java/com/todoList/BackEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoList.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
